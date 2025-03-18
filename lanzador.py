aa649f2eb41507f0e37b7ad0cb65013fdf52bbfd
rubences <91731294+rubences@users.noreply.github.com>
2025-03-18 09:13:15 +0000

    ccs

--- lanzador.py
+++ lanzador.py
@@ -12,21 +12,6 @@ def lanzador_main():
     vehiculos.append(Bicicleta("verde", 2, "bh", "bmx", "urbana"))
     vehiculos.append(Bicicleta("blanca", 2, "orbea", "r2", "deportiva"))
     vehiculos.append(Camioneta("negro", 4, 100, 2200, "ford", "ranger", 1000))
-    vehiculos.append(Moto)
-
-
-
-    def catalogar(vehiculos, ruedas=None):
-        if ruedas is not None:
-            vehiculos_filtrados = [v for v in vehiculos if v.ruedas == ruedas]
-            print(f"Se han encontrado {len(vehiculos_filtrados)} vehículos con {ruedas} ruedas:")
-            for v in vehiculos_filtrados:
-                print(f"{v.__class__.__name__}: {vars(v)}")
-        else:
-            for v in vehiculos:
-                print(f"{v.__class__.__name__}: {vars(v)}")
-
-
-
+    vehiculos.append(Motocicleta("blanca",2,"Suzuki","GSXR", "Deportiva", 300, 999))
 
 
