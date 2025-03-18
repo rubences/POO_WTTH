cf3c6c50759ff278e0a8b9047662a125ad588916
rubences <91731294+rubences@users.noreply.github.com>
2025-03-18 09:26:14 +0000

    casi listo para entregar

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from subclases.coche import Coche
 from subclases.bicicleta import Bicicleta
 from subsubclases.camioneta import Camioneta
 from subsubclases.motocicleta import Motocicleta
-from lanzador import  lanzador_main
+from lanzador import  lanzador_main, catalogar
 
 
 
@@ -14,29 +14,64 @@ def mostrar_menu():
 	print("2. Bicicleta")
 	print("3. Camioneta")
 	print("4. Motocicleta")
-	print("5. Salir")
+	print("5. Ordenar por tipo de ruedas")
+	print("6. Salir")
 
 def obtener_datos_vehiculo(tipo):
 	if tipo == 1:
 		# Obtener datos específicos para Coche
-		marca = input("Ingrese la marca del coche: ")
-		modelo = input("Ingrese el modelo del coche: ")
-		return Coche(marca, modelo)
+		color = input("Ingrese el color del coche: ")
+		ruedas = input("Ingrese el numero de ruedas del coche: ")
+		velocidad = input("Ingrese la velocidad del coche: ")
+		cilindrada = input("Ingrese la cilindrada del coche: ")
+		return Coche(color, ruedas, velocidad, cilindrada)
 	elif tipo == 2:
 		# Obtener datos específicos para Bicicleta
+		color = input("Ingrese el color de la bicicleta: ")
+		ruedas = input("Ingrese el numero de ruedas de la bicicleta: ")
 		marca = input("Ingrese la marca de la bicicleta: ")
-		tipo_bici = input("Ingrese el tipo de bicicleta: ")
-		return Bicicleta(marca, tipo_bici)
+		modelo = input("Ingrese el modelo de la bicicleta: ")
+		tipo = input("Ingrese el tipo de la bicicleta: ")
+		
+		return Bicicleta(color, ruedas, marca, modelo, tipo)
 	elif tipo == 3:
 		# Obtener datos específicos para Camioneta
+		color = input("Ingrese el color de la camioneta: ")
+		ruedas = input("Ingrese el numero de ruedas de la camioneta: ")
+		velocidad = input("Ingrese la velocidad de la camioneta: ")
+		cilindrada = input("Ingrese la cilindrada de la camioneta: ")
 		marca = input("Ingrese la marca de la camioneta: ")
-		capacidad = input("Ingrese la capacidad de la camioneta: ")
-		return Camioneta(marca, capacidad)
+		modelo = input("Ingrese el modelo de la camioneta: ")
+		carga = input("Ingrese la carga de la camioneta: ")
+		
+		return Camioneta(color, ruedas, velocidad, cilindrada, marca, modelo, carga)
 	elif tipo == 4:
 		# Obtener datos específicos para Motocicleta
+		color = input("Ingrese el color de la motocicleta: ")
+		ruedas = input("Ingrese el numero de ruedas de la motocicleta: ")
 		marca = input("Ingrese la marca de la motocicleta: ")
+		modelo = input("Ingrese el modelo de la motocicleta: ")
+		tipo = input("Ingrese el tipo de la motocicleta: ")
+		velocidad = input("Ingrese la velocidad de la motocicleta: ")
 		cilindrada = input("Ingrese la cilindrada de la motocicleta: ")
-		return Motocicleta(marca, cilindrada)
+		
+		return Motocicleta(color, ruedas, marca, modelo, tipo, velocidad, cilindrada)
+	
+	elif tipo == 5:
+		
+		def catalogar(vehiculos, ruedas=None):
+			if ruedas is not None:
+				vehiculos_filtrados = [vehiculo for vehiculo in vehiculos if vehiculo.ruedas == ruedas]
+				print(f"Se han encontrado {len(vehiculos_filtrados)} vehículos con {ruedas} ruedas:")
+				for vehiculo in vehiculos_filtrados:
+					print(vehiculo)
+			else:
+				for vehiculo in vehiculos:
+					print(vehiculo)
+
+		catalogar(vehiculos, 0)
+		catalogar(vehiculos, 2)
+		catalogar(vehiculos, 4)
 	else:
 		return None
 
@@ -44,7 +79,7 @@ if __name__ == "__main__":
 	while True:
 		mostrar_menu()
 		opcion = int(input("Seleccione una opción: "))
-		if opcion == 5:
+		if opcion == 6:
 			break
 		vehiculo = obtener_datos_vehiculo(opcion)
 		if vehiculo:
